0fb4722d08390b10e9a01c0d8891f8f83f16f153
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2025-06-16 20:02:59 +0530

    DONE: - added dependency quickcheck and quickcheckmacros - utilizing quickcheck and quickcheckmacros - implemented Arbitrary trait for ValidEmailFixture - testing email validation 158 1/2

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -31,25 +31,37 @@ mod tests {
     // `.fake` method on `SafeEmail`
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use quickcheck::Gen;
+
+    // Both `Clone` and `Debug` are required by quickcheck
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    // Implementation for `arbitrary` is required as default implementation for `shrink` is already present
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
 
     #[test]
-    fn empty_string_is_rejected() {
+    fn test_empty_string_is_rejected() {
         let email = "".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
     #[test]
-    fn email_missing_at_symbol_is_rejected() {
+    fn test_email_missing_at_symbol_is_rejected() {
         let email = "ursuladomain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
     #[test]
-    fn email_missing_subject_is_rejected() {
+    fn test_email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
     }
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        claim::assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck_macros::quickcheck]
+    fn test_valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
