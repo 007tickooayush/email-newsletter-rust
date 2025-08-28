9a69ff688eaf7fb44aade44fdc924b7e3292f6db
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2025-08-28 20:40:51 +0530

    DONE: - implementing LoginError for handling login related errors 334

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,8 +1,11 @@
+use std::fmt::Formatter;
 use actix_web::http::header::LOCATION;
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, ResponseError};
+use actix_web::http::StatusCode;
 use secrecy::Secret;
 use sqlx::PgPool;
 use crate::authentication::{validate_credentials, Credentials};
+use crate::routes::error_chain_fmt;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,7 +26,7 @@ pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpRe
         Ok(user_id) => {
             tracing::Span::current()
                 .record("user_id", &tracing::field::display(&user_id));
-            
+
             // redirecting to homepage
             // For comprehensive documentation see MDN Web Docs regarding "303 See Other"
             HttpResponse::SeeOther()
@@ -39,4 +42,28 @@ pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpRe
     // HttpResponse::Ok()
     //     .content_type(actix_web::http::header::ContentType::html())
     //     .body(include_str!("login.html"))
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication Failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error)
+}
+
+
+impl std::fmt::Debug for LoginError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
